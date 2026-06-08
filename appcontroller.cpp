ed43b647d0d3d9bdc460fe5530d872939475a47c
mvrc <marcosviniciusrodriguescosta2@gmail.com>
2026-06-08 16:46:19 -0300

    Added more code for the crud operations

--- appcontroller.cpp
+++ appcontroller.cpp
@@ -66,7 +66,7 @@ void AppController::insertIntoInstitutions(QString nome, QString cnpj)
 {
     qDebug() << "Salvo" << nome << cnpj;
     QSqlQuery query;
-    query.prepare("INSERT INTO instituicao(nome,cnpj) VALUES(:nome,:cnpj)");
+    query.prepare("INSERT INTO instituicao(nome,cnpj) VALUES(:nome,:cnpj);");
     query.bindValue(":nome",nome);
     query.bindValue(":cnpj",cnpj);
     bool ok=query.exec();
@@ -80,7 +80,7 @@ void AppController::insertIntoInstitutions(QString nome, QString cnpj)
 void AppController::insertIntoSectors(QString nome,int id_instituicao)
 {
     QSqlQuery query;
-    query.prepare("INSERT INTO setor(nome,id_instituicao) VALUES(:nome,:id_instituicao)");
+    query.prepare("INSERT INTO setor(nome,id_instituicao) VALUES(:nome,:id_instituicao);");
     query.bindValue(":nome",nome);
     query.bindValue(":id_instituicao",id_instituicao);
     bool ok=query.exec();
@@ -94,7 +94,7 @@ void AppController::insertIntoSectors(QString nome,int id_instituicao)
 void AppController::insertIntoEquipments(QString patrimonio, QString modelo, QString fabricante, QString data_aquisição, int id_setor,int id_tipo_equipamento)
 {
     QSqlQuery query;
-    query.prepare("INSERT INTO equipamento(patrimonio,modelo,fabricante,data_aquisicao,id_setor,id_tipo_equipamento) VALUES(:patrimonio,:modelo,:fabricante,:data_aquisicao,:id_setor,:id_tipo_equipamento)");
+    query.prepare("INSERT INTO equipamento(patrimonio,modelo,fabricante,data_aquisicao,id_setor,id_tipo_equipamento) VALUES(:patrimonio,:modelo,:fabricante,:data_aquisicao,:id_setor,:id_tipo_equipamento);");
     query.bindValue(":patrimonio",patrimonio);
     query.bindValue(":modelo",modelo);
     query.bindValue(":fabricante",fabricante);
@@ -112,7 +112,7 @@ void AppController::insertIntoEquipments(QString patrimonio, QString modelo, QSt
 void AppController::insertIntoTipoEquipamentos(QString nome, QString descricao)
 {
     QSqlQuery query;
-    query.prepare("INSERT INTO tipo_equipamento(nome,descricao) VALUES(:nome,:decricao)");
+    query.prepare("INSERT INTO tipo_equipamento(nome,descricao) VALUES(:nome,:decricao);");
     query.bindValue(":nome",nome);
     query.bindValue(":descricao",descricao);
     bool ok=query.exec();
@@ -120,6 +120,132 @@ void AppController::insertIntoTipoEquipamentos(QString nome, QString descricao)
         qDebug() << "saved"<< nome;
     }else {
         qDebug() << "Erro ao salvar equipamento:" << query.lastError().text();
+    }
+}
+
+void AppController::updateInstitutions(int id_instituicao, QString nome, QString cnpj)
+{
+    QSqlQuery query;
+    query.prepare("UPDATE instituicao SET nome=:nome,cnpj=:cnpj WHERE id_instituicao=:id_instituicao");
+    query.bindValue(":nome",nome);
+    query.bindValue(":cnpj",cnpj);
+    query.bindValue(":id_instituicao",id_instituicao);
+    query.exec();
+}
+
+void AppController::updateSectors(int id_setor, QString nome, int id_instituicao)
+{
+    QSqlQuery query;
+    query.prepare("UPDATE setor SET nome=:nome,id_instituicao=:id_instituicao WHERE id_setor=:id_setor");
+    query.bindValue(":nome",nome);
+    query.bindValue(":id_instituicao",id_instituicao);
+    query.bindValue(":id_setor",id_setor);
+    query.exec();
+}
+
+void AppController::updateEquipments(int id_equipamento, QString patrimonio, QString modelo, QString fabricante, QString data_aquisição, int id_setor, int id_tipo_equipamento)
+{
+    QSqlQuery query;
+    query.prepare("UPDATE equipamento SET patrimonio=:patrimonio,modelo=:modelo,fabricante=:fabricante,data_aquisicao=:data_aquisicao,id_setor=:id_setor,id_tipo_equipamentos=:id_tipo_equipamentos WHERE id_equipamento=:id_equipamento");
+    query.bindValue(":patrimonio",patrimonio);
+    query.bindValue(":modelo",modelo);
+    query.bindValue(":fabricante",fabricante);
+    query.bindValue(":data_aquisição",data_aquisição);
+    query.bindValue(":id_setor",id_setor);
+    query.bindValue("id_tipo_equipamento",id_tipo_equipamento);
+    query.exec();
+}
+
+void AppController::updateTipoEquipamentos(int id_tipo_equipamento, QString nome, QString descricao)
+{
+    QSqlQuery query;
+    query.prepare("UPDATE tipo_equipamento SET nome=:nome,descricao=:descricao WHERE id_tipo_equipamento=:id_tipo_equipamento");
+    query.bindValue(":nome",nome);
+    query.bindValue(":descricao",descricao);
+    query.bindValue(":id_tipo_equipamento",id_tipo_equipamento);
+    query.exec();
+}
+
+void AppController::deleteInstitutions(int id_instituicao)
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM instituicao WHERE id_instituicao=:id_instituicao");
+    query.bindValue(":id_instituicao",id_instituicao);
+    query.exec();
+}
+
+void AppController::deleteSectors(int id_setor)
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM setor WHERE id_instituicao=:id_setor");
+    query.bindValue(":id_instituicao",id_setor);
+    query.exec();
+}
+
+void AppController::deleteEquipments(int id_equipamento)
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM equipamento WHERE id_equipamento=:id_equipamento");
+    query.bindValue(":id_equipamento",id_equipamento);
+    query.exec();
+}
+
+void AppController::deleteTipoEquipamentos(int id_tipo_equipamento)
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM tipo_equipamento WHERE id_tipo_equipamento=:id_tipo_equipamento");
+    query.bindValue(":id_tipo_equipamento",id_tipo_equipamento);
+    query.exec();
+}
+
+void AppController::searchInstitutions()
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM instituicao;");
+    query.exec();
+    while (query.next()) {
+        int id_instituicao=query.value(0).toInt();
+        QString nome=query.value(1).toString();
+        QString cnpj=query.value(2).toString();
+    }
+}
+
+void AppController::searchSectors()
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM setor;");
+    query.exec();
+    while (query.next()) {
+        int id_setor=query.value(0).toInt();
+        QString nome_setor=query.value(1).toString();
+    }
+}
+
+void AppController::searchEquipments()
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM equipamento;");
+    query.exec();
+    while (query.next()) {
+        int id_equipamento=query.value(0).toInt();
+        QString patrimonio=query.value(1).toString();
+        QString modelo=query.value(2).toString();
+        QString fabricante=query.value(3).toString();
+        QString data_de_aquisicao=query.value(4).toString();
+        int fk_id_setor=query.value(5).toInt();
+        int fk_id_tipo_equipamento=query.value(6).toInt();
+    }
+}
+
+void AppController::searchTipoEquipamentos()
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM tipo_equipamento;");
+    query.exec();
+    while (query.next()) {
+        int id_tipo_equipamento=query.value(0).toInt();
+        QString nome=query.value(1).toString();
+        QString descricao=query.value(2).toString();
     }
 }
 
--- appcontroller.h
+++ appcontroller.h
@@ -25,14 +25,18 @@ public:
     Q_INVOKABLE void insertIntoSectors(QString nome,int id_instituicao);
     Q_INVOKABLE void insertIntoEquipments(QString patrimonio,QString modelo,QString fabricante,QString data_aquisição,int id_setor,int id_tipo_equipamento);
     Q_INVOKABLE void insertIntoTipoEquipamentos(QString nome, QString descricao);
-    Q_INVOKABLE void updateInstitutions(QString nome,QString cnpj);
-    Q_INVOKABLE void updateSectors(QString nome,int id_instituicao);
-    Q_INVOKABLE void updateEquipments(QString patrimonio,QString modelo,QString fabricante,QString data_aquisição,int id_setor,int id_tipo_equipamento);
-    Q_INVOKABLE void updateTipoEquipamentos(QString nome, QString descricao);
-    Q_INVOKABLE void deleteInstitutions(QString nome,QString cnpj);
-    Q_INVOKABLE void deleteSectors(QString nome,int id_instituicao);
-    Q_INVOKABLE void deleteEquipments(QString patrimonio,QString modelo,QString fabricante,QString data_aquisição,int id_setor,int id_tipo_equipamento);
-    Q_INVOKABLE void deleteTipoEquipamentos(QString nome, QString descricao);
+    Q_INVOKABLE void updateInstitutions(int id_instituicao,QString nome,QString cnpj);
+    Q_INVOKABLE void updateSectors(int id_setor,QString nome,int id_instituicao);
+    Q_INVOKABLE void updateEquipments(int id_equipamento,QString patrimonio,QString modelo,QString fabricante,QString data_aquisição,int id_setor,int id_tipo_equipamento);
+    Q_INVOKABLE void updateTipoEquipamentos(int id_tipo_equipamento,QString nome, QString descricao);
+    Q_INVOKABLE void deleteInstitutions(int id_instituicao);
+    Q_INVOKABLE void deleteSectors(int id_setor);
+    Q_INVOKABLE void deleteEquipments(int id_equipamento);
+    Q_INVOKABLE void deleteTipoEquipamentos(int id_tipo_equipamento);
+    Q_INVOKABLE void searchInstitutions();
+    Q_INVOKABLE void searchSectors();
+    Q_INVOKABLE void searchEquipments();
+    Q_INVOKABLE void searchTipoEquipamentos();
     QString instituicao() const;
     void setInstituicao(const QString &value);
 
